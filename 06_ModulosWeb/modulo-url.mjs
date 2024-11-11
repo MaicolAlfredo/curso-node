d5603e8943d1193b365a9ffb4b97c07dcb47be81
MaicolAlfredo <tegnoblog.17alfrdo@gmail.com>
2024-11-11 00:10:21 +0000

    Modulo URL

---
+++ 06_ModulosWeb/modulo-url.mjs
@@ -0,0 +1,11 @@
+import { parse } from "url";
+
+const urlString =
+  "https://www.ejemplo.com:8080/ruta?parametro1=valor1&parametro2=valor2";
+
+const parsedURL = parse(urlString, true);
+
+console.log("Protocolo:",parsedURL.protocol)
+console.log("Hostname:",parsedURL.hostname)
+console.log("Pathname:", parsedURL.pathname)
+console.log("Parámetros de consulta:", parsedURL.query);
